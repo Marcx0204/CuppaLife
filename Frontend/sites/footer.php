f46f527a5c961c64ff7769c30cff2103dec84ba0
Marc Simoneit <wi21b112@technikum-wien.at>
2023-05-10 13:08:19 +0200

    first commit

---
+++ Frontend/sites/footer.php
@@ -0,0 +1,11 @@
+<div class="container">
+  <footer class="py-3 my-4">
+		<ul class="nav justify-content-center border-top pb-3 mb-auto ">
+			<li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
+			<li class="nav-item"><a href="impressum.php" class="nav-link px-2 text-muted">Impressum</a></li>
+			<li class="nav-item"><a href="help.php" class="nav-link px-2 text-muted">Hilfe</a></li>
+			<li class="nav-item"><a href="contact.php" class="nav-link px-2 text-muted">Kontakt</a></li>
+		</ul>
+		<p class="text-center text-muted mt-auto">&copy; 2022 Company, Inc</p>
+	</footer>
+</div>
